eacf54abeccaa2a21ecbbdb44ab516865ec9bd5d
Atul Varma <varmaa@gmail.com>
2020-01-09 07:45:32 -0500

    Add state.

--- app.ts
+++ app.ts
@@ -2,17 +2,31 @@ import express from 'express';
 
 type ConversationStatus = "end"|"continue";
 
+type ConversationState = {
+  counter: number,
+};
+
 type TextitResponse = {
   text: string,
   conversationStatus: ConversationStatus,
+  state: string,
 };
 
 const PORT = process.env.PORT || '3000';
 
 const app = express();
 
+function parseState(value: string|undefined, defaultValue: ConversationState): ConversationState {
+  try {
+    return JSON.parse(value || '');
+  } catch (e) {
+    return defaultValue;
+  }
+}
+
 app.get('/', (req, res) => {
   const input: string|undefined = req.query.input;
+  const state = parseState(req.query.state, {counter: 0});
   let text: string;
   let conversationStatus: ConversationStatus = "continue";
 
@@ -21,13 +35,14 @@ app.get('/', (req, res) => {
       text = "Okay, bye.";
       conversationStatus = "end";
     } else {
-      text = `Well ${input} to you too.`;
+      text = `Well ${input} to you too (${state.counter}).`;
+      state.counter++;
     }
   } else {
     text = "Welcome, buddy!";
   }
 
-  const response: TextitResponse = {text, conversationStatus};
+  const response: TextitResponse = {text, conversationStatus, state: JSON.stringify(state)};
   res.send(response);
 });
 
