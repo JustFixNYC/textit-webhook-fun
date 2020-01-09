410cafc9c793da34e234dcfb037185f3ca27794f
Atul Varma <varmaa@gmail.com>
2020-01-09 09:55:57 -0500

    Perform geosearch.

---
+++ geosearch.ts
@@ -0,0 +1,80 @@
+import fetch from 'isomorphic-fetch';
+
+/**
+ * For documentation about this endpoint, see:
+ *
+ * https://geosearch.planninglabs.nyc/docs/#autocomplete
+ */
+export const GEO_AUTOCOMPLETE_URL = 'https://geosearch.planninglabs.nyc/v1/autocomplete';
+
+/**
+ * The keys here were obtained experimentally, I'm not actually sure
+ * if/where they are formally specified.
+ */
+export enum GeoSearchBoroughGid {
+  Manhattan = 'whosonfirst:borough:1',
+  Bronx = 'whosonfirst:borough:2',
+  Brooklyn = 'whosonfirst:borough:3',
+  Queens = 'whosonfirst:borough:4',
+  StatenIsland = 'whosonfirst:borough:5',
+}
+
+/**
+ * This is what the NYC Geosearch API returns from its
+ * autocomplete endpoint.
+ * 
+ * Note that some of the fields are "unknown", which
+ * just implies that they exist but we're not really
+ * sure what type they are (nor do we particularly
+ * care, at the moment, for our purposes).
+ */
+export interface GeoSearchResults {
+  bbox: unknown;
+  features: GeoSearchFeature[];
+}
+
+export interface GeoSearchFeature {
+  geometry: unknown;
+  properties: GeoSearchProperties
+}
+
+/**
+ * Note that these are by no means all the
+ * properties, they're just the ones we care about.
+ */
+export interface GeoSearchProperties {
+  /** e.g. "Brooklyn" */
+  borough: string;
+
+  /** e.g. "whosonfirst:borough:2" */
+  borough_gid: GeoSearchBoroughGid;
+
+  /** e.g. "150" */
+  housenumber: string;
+
+  /** e.g. "COURT STREET" */
+  street: string;
+
+  /** e.g. "150 COURT STREET" */
+  name: string;
+
+  /** e.g. "150 COURT STREET, Brooklyn, New York, NY, USA" */
+  label: string;
+
+  /**
+   * The 10-digit padded Borough-Block-Lot (BBL) number for the
+   * property, e.g. "3002920026".
+   */
+  pad_bbl: string;
+
+  /** The ZIP code, e.g. "11201". */
+  postalcode: string;
+}
+
+export async function geosearch(text: string): Promise<GeoSearchResults> {
+  const res = await fetch(`${GEO_AUTOCOMPLETE_URL}?text=${encodeURIComponent(text)}`);
+  if (res.status !== 200) {
+    throw new Error(`Received HTTP ${res.status}!`);
+  }
+  return res.json();
+}
--- state-handlers.ts
+++ state-handlers.ts
@@ -1,16 +1,19 @@
 import { TextitConversationStatus, TextitResponse } from "./textit";
+import { geosearch, GeoSearchBoroughGid } from "./geosearch";
 
 type StateHandlerName = (keyof StateHandlers)|'END';
 
 type State = {
   handlerName: StateHandlerName,
+  boroughGid?: GeoSearchBoroughGid,
+  zip?: string,
 };
 
 type StateHandler = (state: State, input: string) => TextitResponse|Promise<TextitResponse>;
 
 class StateHandlers {
   intro1: StateHandler = s => {
-    return say([
+    return say(s, [
       `Right to Counsel is a new law in NYC that provides free legal representation for eligible tenants. You may qualify based on:`,
       `- where you live in NYC`,
       `- income and household size`,
@@ -20,13 +23,40 @@ class StateHandlers {
 
   intro2: StateHandler = s => {
     return ask(
+      s,
       `Let's see if you have the right to a free attorney! To start, what is your address and borough? Example: 654 Park Place, Brooklyn`,
       'receiveContactAddress'
     );
   };
 
   receiveContactAddress: StateHandler = async (s, input) => {
-    return end(`TODO look up "${input}" in geosearch!`);
+    const results = await geosearch(input);
+    if (!results.features.length) {
+      return ask(
+        s,
+        `Hmm, we couldn't understand that address. Can you try being more specific?`,
+        'receiveContactAddress'
+      );
+    }
+    const props = results.features[0].properties;
+    return ask(s, [
+      `Is this your address?`,
+      props.label,
+      `Please reply with either Yes or No.`
+    ], 'confirmAddress', {
+      boroughGid: props.borough_gid,
+      zip: props.postalcode,
+    });
+  };
+
+  confirmAddress: StateHandler = (s, input) => {
+    if (input.toLowerCase().startsWith('y')) {
+      return end(s, `TODO: Finish this! User's borough is ${s.boroughGid} and zip is ${s.zip}.`);
+    } else if (input.toLowerCase().startsWith('n')) {
+      return say(s, "Oops, let's try again!", 'intro2');
+    } else {
+      return ask(s, `Sorry, I didn't understand that. Please respond with Yes or No.`, 'confirmAddress');
+    }
   };
 }
 
@@ -50,8 +80,10 @@ export async function handleState(input: string, deserializedState: Object|null)
   return handler(state, input);
 };
 
-function response(text: string|string[], nextStateHandler: StateHandlerName, status: TextitConversationStatus): TextitResponse {
+function response(s: State, text: string|string[], nextStateHandler: StateHandlerName, status: TextitConversationStatus, stateUpdates?: Partial<State>): TextitResponse {
   const nextState: State = {
+    ...s,
+    ...stateUpdates,
     handlerName: nextStateHandler,
   };
 
@@ -62,14 +94,14 @@ function response(text: string|string[], nextStateHandler: StateHandlerName, sta
   return {text, conversationStatus: status, state: JSON.stringify(nextState)};
 }
 
-function ask(text: string|string[], nextStateHandler: StateHandlerName): TextitResponse {
-  return response(text, nextStateHandler, 'ask');
+function ask(s: State, text: string|string[], nextStateHandler: StateHandlerName, stateUpdates?: Partial<State>): TextitResponse {
+  return response(s, text, nextStateHandler, 'ask', stateUpdates);
 }
 
-function say(text: string|string[], nextStateHandler: StateHandlerName): TextitResponse {
-  return response(text, nextStateHandler, 'loop');
+function say(s: State, text: string|string[], nextStateHandler: StateHandlerName, stateUpdates?: Partial<State>): TextitResponse {
+  return response(s, text, nextStateHandler, 'loop', stateUpdates);
 }
 
-function end(text: string|string[]): TextitResponse {
-  return response(text, 'END', 'end');
+function end(s: State, text: string|string[]): TextitResponse {
+  return response(s, text, 'END', 'end');
 }
